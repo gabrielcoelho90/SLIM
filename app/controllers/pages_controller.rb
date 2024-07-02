c83189e8640a53afb05f1b94a0add9d9c799f399
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-07-02 12:44:01 -0300

    Created calculators of BMI and BMR

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,40 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def info
+    @user_age = params[:age].to_i
+    @user_weight = params[:peso].to_f
+    @user_height = params[:altura].to_f
+    @user_gender = params[:gender]
+    @metric = params[:metric]
+    @time = params[:time].to_i
+    @time = day_convert(@metric, @time) unless @metric == "Days"
+    @kilos = params[:kilos].to_f
+    @calories_per_day = deficit(@kilos, @time).round(2)
+    @bmi = bmi(@user_weight, @user_height).round(2)
+    @bmr = baseline_calories(@user_gender, @user_height, @user_weight, @user_age).round(2)
+  end
+
+  def bmi(weight, height)
+    weight / (height * height)
+  end
+
+  def baseline_calories(gender, height, weight, age)
+    if gender == 'male'
+      88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    else
+      447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+    end
+  end
+
+  # calorias por dia que preciso perder
+  def deficit(kilos, time)
+    (kilos * 7700) / time
+  end
+
+  # converter para dias
+  def day_convert(metric, time)
+    metric == "Months" ? time * 30 : time * 7
+  end
 end
