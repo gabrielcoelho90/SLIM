83bd4d2fa0330873dc5311397dbe2c1df7e16e09
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-07-24 19:03:20 -0300

    created a _form.html.erb partial and minors adjustments

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_143615) do
     t.string "gender"
     t.string "name"
     t.float "kilos_to_lose"
-    t.datetime "timeline"
+    t.float "timeline"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
